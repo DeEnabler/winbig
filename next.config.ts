1cccecc8f469f145e249873dd30d7fada97b2d01
deenabler <c.t.o.mainshit@gmail.com>
2025-07-08 06:17:08 +0300

    fix: remove invalid experimental config from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -19,11 +19,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    allowedDevOrigins: [
-      "https://9000-firebase-studio-1748227192070.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev",
-    ],
-  },
 };
 
 export default nextConfig;
