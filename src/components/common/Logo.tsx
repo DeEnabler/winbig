982a969ed49cb3a5548ab71b9201687e0339be05
deenabler <c.t.o.mainshit@gmail.com>
2025-12-29 12:23:42 +0200

    Add WinBig logo and set as site icon
    - Added transparent PNG and JPEG logo assets to public folder
    - Updated Logo component to display the WinBig logo
    - Set up site favicon using the new logo

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,9 +1,17 @@
 
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center space-x-2">
+      <Image 
+        src="/logo.png" 
+        alt="WinBig Logo" 
+        width={32} 
+        height={32}
+        className="object-contain"
+      />
       <span className="font-extrabold text-2xl text-primary">
         WinBig
       </span>
